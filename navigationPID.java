183f125e3a7ddc7bdb75beae6e939218359c430d
AtomicRobotics3805 <AtomicRobotics3805@gmail.com>
2016-09-08 12:57:13 -0700

    Update navigationPID.java

--- navigationPID.java
+++ navigationPID.java
@@ -9,12 +9,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
 
-public final class navigationPID {
+public final class navigationController {
     private AdafruitIMU AdafruitGyro; //Instance of AdafruitIMU
     private volatile double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2];  //Array to store IMU's output
 
     private DcMotor leftMotor; //Instance of DcMotor
     private DcMotor rightMotor; //Instance of DcMotor
+
+    private double[][] movementArray;
+    private int movementArrayStep = 0;
 
     //Variables for PID loop
     private double setPoint = 0;
@@ -29,12 +32,8 @@ public final class navigationPID {
     private double encoderTicksPerInch = encoderTicksPerRev / (Math.PI * 3); //Variable to convert encoder ticks to inches
     private int encoderPositionReference; //Variable to use as reference for moveForward and moveBackward
 
-    private int currentMovement = 0; //Variable to choose which methods to call in movementControl and readyForNewCommand. This is set to 0 so readyForNewCommand initially goes to the default case
-    private double currentGoalValue; //Variable to store the parameters passed through move
-    private double currentTpValue; //Variable to store the parameters passed through move
-
     //Constructor
-    public navigationPID(HardwareMap currentIMUHWmap, String configuredIMUname, DcMotor leftMotor, DcMotor rightMotor) {
+    public navigationController(double [][] movementCommandArray, HardwareMap currentIMUHWmap, String configuredIMUname, DcMotor leftMotor, DcMotor rightMotor) {
         //Map the IMU instance pointer to the actual hardware
         try {
             AdafruitGyro = new AdafruitIMU(currentIMUHWmap, configuredIMUname, (byte) (AdafruitIMU.BNO055_ADDRESS_A * 2),
@@ -46,6 +45,8 @@ public final class navigationPID {
         //Assign the DcMotor instances to those of the main class
         this.leftMotor = leftMotor;
         this.rightMotor = rightMotor;
+
+        movementArray = movementCommandArray;
     }
 
     //**********Public methods***********
@@ -63,44 +64,25 @@ public final class navigationPID {
         Kd = newKd;
     }
 
-    public boolean readyForNewCommand() {
-        boolean readyForNextValue; //Local variable to store the output of the switch statement
-        switch (currentMovement) {
-            case 1:
-                readyForNextValue = moveForward(currentGoalValue, currentTpValue); //Set readyForNextValue to the output of moveForward with the same arguments from the last time move was called
-                break;
-            case 2:
-                readyForNextValue = moveBackward(currentGoalValue, currentTpValue); //Set readyForNextValue to the output of moveBackward with the same arguments from the last time move was called
-                break;
-            case 3:
-                readyForNextValue = rotateCW(currentGoalValue, currentTpValue); //Set readyForNextValue to the output of rotateCW with the same arguments from the last time move was called
-                break;
-            case 4:
-                readyForNextValue = rotateCCW(currentGoalValue, currentTpValue); //Set readyForNextValue to the output of rotateCCW with the same arguments from the last time move was called
-                break;
-            case 5:
-                readyForNextValue = false; //5 is the end navigation value, so set readyForNextValue to false
-                break;
-            default:
-                readyForNextValue = true; //If there is any other value, set readyForNextValue to true
-                break;
-        }
-
-        return readyForNextValue; //Send the output of the switch statement
+    public void loopNavigation() {
+        move((int) movementArray[movementArrayStep][0], movementArray[movementArrayStep][1], movementArray[movementArrayStep][2]);
     }
 
-    public void move(int movementType, double Tp, double goalDistanceOrAngle) {
-        //Store the current arguments so the user does not have to respecify these values until the next time move is called
-        currentGoalValue = goalDistanceOrAngle;
-        currentTpValue = Tp;
-        currentMovement = movementType;
+    public int navigationStep() {
+        return movementArrayStep;
+    }
 
-        movementControl(movementType, Tp, goalDistanceOrAngle); //Call movementControl
+    public int navigationType() {
+        return (int) movementArray[movementArrayStep][0];
+    }
+
+    public double currentHeading() {
+        return yawAngle[0];
     }
 
     //**********Private methods**********
 
-    private void movementControl(int movementType, double Tp, double goalValue) {
+    private void move(int movementType, double Tp, double goalValue) {
         switch (movementType) {
             case 1:
                 moveForward(goalValue, Tp);
@@ -120,6 +102,51 @@ public final class navigationPID {
         }
     }
 
+    private void moveForward(double goalDistanceInch, double Tp) { //Movement val == 1
+        int distanceTraveled = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
+
+        if (distanceTraveled > (encoderTicksPerInch * goalDistanceInch) + encoderPositionReference) {
+            loopPID(Tp);
+        } else {
+            nextMovement();
+        }
+    }
+
+    private void moveBackward(double goalDistanceInch, double Tp) { //Movement val == 1
+        int distanceTraveled = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
+
+        if (distanceTraveled < (encoderTicksPerInch * goalDistanceInch) + encoderPositionReference) {
+            loopPID(Tp);
+        } else {
+            nextMovement();
+        }
+    }
+
+    private void rotateCW(double goalAngle, double Tp) { //Movement val == 3
+        setPoint = goalAngle;
+        if (yawAngle[0] > setPoint) {
+            loopPID(Tp);
+        } else {
+            nextMovement();
+        }
+    }
+
+    private void rotateCCW(double goalAngle, double Tp) { //Movement val == 4
+        setPoint = goalAngle;
+        if (yawAngle[0] < setPoint) {
+            loopPID(Tp);
+        } else {
+            nextMovement();
+        }
+    }
+
+    private void nextMovement() {
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        encoderPositionReference = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
+        movementArrayStep++;
+    }
+
     private double loopPID(double Tp) {
         AdafruitGyro.getIMUGyroAngles(rollAngle, pitchAngle, yawAngle);
 
@@ -136,51 +163,5 @@ public final class navigationPID {
         preError = error;
 
         return output;
-    }
-
-    private boolean moveForward(double goalDistanceInch, double Tp) { //Movement val == 1
-        int distanceTraveled = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
-
-        if (distanceTraveled > (encoderTicksPerInch * goalDistanceInch) + encoderPositionReference) {
-            loopPID(Tp);
-            return false;
-        } else {
-            encoderPositionReference = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
-            return true;
-        }
-    }
-
-    private boolean moveBackward(double goalDistanceInch, double Tp) { //Movement val == 1
-        int distanceTraveled = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
-
-        if (distanceTraveled < (encoderTicksPerInch * goalDistanceInch) + encoderPositionReference) {
-            loopPID(Tp);
-            return false;
-        } else {
-            encoderPositionReference = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
-            return true;
-        }
-    }
-
-    private boolean rotateCW(double goalAngle, double Tp) { //Movement val == 3
-        setPoint = goalAngle;
-        if (yawAngle[0] > setPoint) {
-            loopPID(Tp);
-            return false;
-        } else {
-            encoderPositionReference = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
-            return true;
-        }
-    }
-
-    private boolean rotateCCW(double goalAngle, double Tp) { //Movement val == 4
-        setPoint = goalAngle;
-        if (yawAngle[0] < setPoint) {
-            loopPID(Tp);
-            return false;
-        } else {
-            encoderPositionReference = (leftMotor.getCurrentPosition() + rightMotor.getCurrentPosition()) / 2;
-            return true;
-        }
     }
 }
